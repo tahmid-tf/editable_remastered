6cd29556499d95942dff3053972c83036717b3d2
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2025-07-01 15:48:32 +0600

    Add Livewire package and initial configuration
    Installed the livewire/livewire package via Composer and added its configuration file at config/livewire.php. Updated the dashboard layout to include @livewireStyles and @livewireScripts for proper asset loading. This sets up the project for Livewire component development.

--- resources/views/layouts/dashboard/main.blade.php
+++ resources/views/layouts/dashboard/main.blade.php
@@ -30,6 +30,8 @@
         src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js"
         crossorigin="anonymous"
     ></script>
+
+    @livewireStyles
 </head>
 <body class="nav-fixed">
 <nav
@@ -229,25 +231,25 @@
                 <div class="nav accordion" id="accordionSidenav">
                     <!-- Sidenav Menu Heading (Account)-->
                     <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-{{--                    <div class="sidenav-menu-heading d-sm-none">Account</div>--}}
+                    {{--                    <div class="sidenav-menu-heading d-sm-none">Account</div>--}}
                     <!-- Sidenav Link (Alerts)-->
                     <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-{{--                    <a class="nav-link d-sm-none" href="#!">--}}
-{{--                        <div class="nav-link-icon"><i data-feather="bell"></i></div>--}}
-{{--                        Alerts--}}
-{{--                        <span class="badge bg-warning-soft text-warning ms-auto"--}}
-{{--                        >4 New!</span--}}
-{{--                        >--}}
-{{--                    </a>--}}
+                    {{--                    <a class="nav-link d-sm-none" href="#!">--}}
+                    {{--                        <div class="nav-link-icon"><i data-feather="bell"></i></div>--}}
+                    {{--                        Alerts--}}
+                    {{--                        <span class="badge bg-warning-soft text-warning ms-auto"--}}
+                    {{--                        >4 New!</span--}}
+                    {{--                        >--}}
+                    {{--                    </a>--}}
                     <!-- Sidenav Link (Messages)-->
                     <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-{{--                    <a class="nav-link d-sm-none" href="#!">--}}
-{{--                        <div class="nav-link-icon"><i data-feather="mail"></i></div>--}}
-{{--                        Messages--}}
-{{--                        <span class="badge bg-success-soft text-success ms-auto"--}}
-{{--                        >2 New!</span--}}
-{{--                        >--}}
-{{--                    </a>--}}
+                    {{--                    <a class="nav-link d-sm-none" href="#!">--}}
+                    {{--                        <div class="nav-link-icon"><i data-feather="mail"></i></div>--}}
+                    {{--                        Messages--}}
+                    {{--                        <span class="badge bg-success-soft text-success ms-auto"--}}
+                    {{--                        >2 New!</span--}}
+                    {{--                        >--}}
+                    {{--                    </a>--}}
 
                     <!-- Sidenav Menu Heading (Core)-->
                     <div class="sidenav-menu-heading">Core</div>
@@ -2934,5 +2936,7 @@
     crossorigin="anonymous"
 ></script>
 <script src="{{ asset('js/litepicker.js') }}"></script>
+
+@livewireScripts
 </body>
 </html>
